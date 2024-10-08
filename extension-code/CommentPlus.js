c1477c8105bb442fa4fbaa282bff821f31e8facd
Mastered YT Aditya <97113704+MYTAditya@users.noreply.github.com>
2024-10-08 20:42:23 +0530

    Update CommentPlus.js

--- extension-code/CommentPlus.js
+++ extension-code/CommentPlus.js
@@ -376,7 +376,7 @@
     }
     
     commentCPlusPlus(args, util) {
-      util.startBranch(1, true) && util.startBranch(2, true);
+      util.startBranch(1, false) && util.startBranch(2, false);
     }
   }
   Scratch.extensions.register(new CommentPlus());
